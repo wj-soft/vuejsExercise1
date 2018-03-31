63408a24123e26827e2b8aae6d21a354ce40584f
wj-soft <wooju_lee@daum.net>
2018-03-31 17:24:52 +0900

    view

---
+++ src/components/card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card" style="width: 20rem;">
+  <img src="https://picsum.photos/768" alt="Card example image">
+
+  <div class="card-body">
+    <h4 class="card-title">제목</h4>
+    <p class="card-text">Notice that the card width in this example have been set to 20rem, otherwise it will try to fill the current container/row where the card is.</p>
+    <button>Delete</button>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
